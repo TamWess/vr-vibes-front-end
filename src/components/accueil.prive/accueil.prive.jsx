7e2172ad7d7e415edeec2a5a101990426350532b
TamWess <adilsontamrabet@aol.fr>
2023-10-04 00:42:55 +0200

    remove temporaire des titres accueils

--- src/components/accueil.prive/accueil.prive.jsx
+++ src/components/accueil.prive/accueil.prive.jsx
@@ -3,25 +3,25 @@ import "./accueil.prive.scss"
 
 function AccueilPrive() {
 
-	useEffect(() => {
-		setTimeout(() => {
-			const h1EntreprisePrive = document.querySelector(".activitiesPrive")
-			// const h1EntreprisePrive = document.querySelector(".titleAccueilFamilleEntreprise")
-			h1EntreprisePrive.style.opacity = "1";
-			h1EntreprisePrive.classList.add("animate__animated");
-			h1EntreprisePrive.classList.add("animate__fadeInUp");
-		}
-		, 3500
-		)
-	})
+	// useEffect(() => {
+	// 	setTimeout(() => {
+	// 		const h1EntreprisePrive = document.querySelector(".activitiesPrive")
+	// 		// const h1EntreprisePrive = document.querySelector(".titleAccueilFamilleEntreprise")
+	// 		h1EntreprisePrive.style.opacity = "1";
+	// 		h1EntreprisePrive.classList.add("animate__animated");
+	// 		h1EntreprisePrive.classList.add("animate__fadeInUp");
+	// 	}
+	// 	, 3500
+	// 	)
+	// })
 
 	return (
 		<div className="homeAF">
-			<div className="activitiesPrive">
+			{/* <div className="activitiesPrive">
 				<h2 className="babyShower">Baby-Shower</h2>
 				<h2 className="anniversaire">Anniversaire</h2>
 				<h2 className="mariage">Mariage</h2>
-			</div>
+			</div> */}
 			<img className="logoVRvibesAF" src="/icns/vr-vibes-logo.png" alt="logo vrvibes entreprise d'animation en réalité virtuelle sur paris" />
 			<div className="imageVRFamilleAF1 animate__animated animate__backInLeft" />
 			<div className="imageVRFamilleAF2 animate__animated animate__backInDown" />
